1f1be488ae8f7f32db2d930511c0e12775edaba2
Gopi Kishan <kishan.gopi14.gk@gmail.com>
2019-01-29 20:00:42 +0530

    New Implementation

--- balancebot-project/test.py
+++ balancebot-project/test.py
@@ -1,28 +1,37 @@
 import gym
 from baselines import deepq
 import balance_bot
+import tensorflow as tf
+
 
 def callback(lcl, glb):
     # stop training if reward exceeds 199
     is_solved = lcl['t'] > 100 and sum(lcl['episode_rewards'][-101:-1]) / 100 >= 199
     return is_solved
 
+
+
 def main():
     # create the environment
     env = gym.make("balancebot-v0") # <-- this we need to create
 
     # create the learning agent
-    model = deepq.models.mlp([16, 16])
+    # model = deepq.models.mlp([16, 16])
+
+
+    model = deepq.simple.load('balance.pkl')
+    print("Model loaded")
 
     # train the agent on the environment
     act = deepq.learn(
         env, q_func=model, lr=1e-3,
-        max_timesteps=2000, buffer_size=50000, exploration_fraction=0.1,
+        max_timesteps=200000, buffer_size=50000, exploration_fraction=0.1,
         exploration_final_eps=0.02, print_freq=10, callback=callback
     )
+    # save trained model
 
-    ## save trained model
-    act.save("bal.pkl")
+
+    act.save("balance2.pkl")
     
 if __name__ == '__main__':
     main()
