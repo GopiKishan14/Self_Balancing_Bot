52ebb17fedd6d1b27806fcee495eee235a833b79
Rishabh499 <ris499@gmail.com>
2019-03-25 16:35:27 +0530

    updated Robot Environment

--- Real World Robot/env_robo.py
+++ Real World Robot/env_robo.py
@@ -13,22 +13,34 @@ class env_robo_class:
     def observation_space(self,act):
         self.pitch , self.Gx = mpu.observation()
         self.speed= compute_action.controller(act)
-
+        
         return np.array([[self.pitch, self.Gx, self.speed]])
     def step(self,act):
         state=self.observation_space(act)
         reward=self._compute_reward()
         done = self._compute_done()
         self._envStepCounter += 1
-
+        
         return state,reward,done,{}
-
-    def _compute_reward(self):
     
+    def _compute_reward(self):
+        
         # could also be pi/2 - abs(cubeEuler[0])
         return (1 - self.pitch) * 0.1 -  abs(self.speed - self.vd) * 0.01
     def _compute_done(self):
         #please find optimal pitch
-        return self.pitch>1 or self._envStepCounter >= 1500
+        return self.pitch> .9 or self._envStepCounter >= 1500
     def reset(self):
+        self.vt = 0
+        self.speed = 0
+        self._envStepCounter = 0
+        self.pitch , self.Gx = 0,0
+
+
+
+
+
+
+
+
 
